fbbab7937042f5fe8766bdc3c1f7846bb3e2d906
Mohamed Osama <67656249+moOsama76@users.noreply.github.com>
2025-01-07 19:04:25 +0200

    Update generic_inputs.rs

--- generic_inputs.rs
+++ generic_inputs.rs
@@ -1,9 +1,15 @@
 use std::io;
 use std::str::FromStr;
 
-fn take_number<T>() -> T
+#[allow(dead_code)]
+enum ArrayFormate {
+    Inline,
+    NotInline,
+}
+
+fn take_item<T>() -> T
 where
-    T: FromStr,
+    T: FromStr /*+ std::fmt::Debug*/,
     T::Err: std::fmt::Debug,
 {
     let mut s = String::new();
@@ -11,31 +17,42 @@ where
     s.trim().parse().unwrap()
 }
 
-fn take_array<T>(n: u32) -> Vec<T>
+fn take_array<T>(n: u32, array_formate: ArrayFormate) -> Vec<T>
 where
-    T: FromStr,
+    T: FromStr /*+ std::fmt::Debug*/,
     T::Err: std::fmt::Debug, // Ensure the type implements FromStr and provides a debug error
 {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let v: Vec<T> = input
-        .trim()
-        .split_whitespace()
-        .map(|x| x.parse::<T>().unwrap()) // Parse input into generic type
-        .collect();
-    
+    let mut v: Vec<T> = Vec::new();
+    match array_formate {
+        ArrayFormate::Inline => {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            v = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse::<T>().unwrap()) // Parse input into generic type
+                .collect();
+        }
+        ArrayFormate::NotInline => {
+            // We don't need to read an extra line before the loop
+            for _ in 0..n {
+                let mut s = String::new();
+                std::io::stdin().read_line(&mut s).unwrap();
+                v.push(s.trim().parse::<T>().unwrap()); // Parse input into generic type and push to vector
+            }
+        }
+    }
     if v.len() as u32 > n {
         panic!("You added additional {:?} items", v.len() as u32 - n);
     } else if n > v.len() as u32 {
         panic!("You missed remaining {:?} items", n - v.len() as u32);
     }
-
     v
 }
 
 fn main() {
-    let n: u32 = take_number();
-    let v: Vec<u32> = take_array(n);
-    let m: u32 = take_number();
-    print!("{:?} \n{:?}\n{:?}", n, v, m);
+    let n: u32 = take_item();
+    let v: Vec<String> = take_array(n, ArrayFormate::Inline);
+    let m: u32 = take_item();
+    print!("{:?}\n{:?}\n{:?}", n, v, m);
 }
