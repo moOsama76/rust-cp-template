faf9ebc4e5e9e4199ead8d4e55ef89e53a897459
Mohamed Osama <67656249+moOsama76@users.noreply.github.com>
2025-01-08 17:41:21 +0200

    Update and rename single_generic_input to single_generic_input.rs

---
+++ single_generic_input.rs
@@ -0,0 +1,17 @@
+
+use std::io;
+use std::str::FromStr;
+fn take_item<T>() -> T
+where
+    T: FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    s.trim().parse().unwrap()
+}
+
+fn main() {
+    let n: u32 = take_item();
+    print!("{:?}", n);
+}
